7d196c42022fc0c7d6b5743f58d684be25db2f53
Virendra <patelvirendra62@gmail.com>
2020-04-11 08:35:45 +0530

    form validation

--- src/controllers/AdminStudentTab.java
+++ src/controllers/AdminStudentTab.java
@@ -12,6 +12,7 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
+import org.controlsfx.control.Notifications;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -47,6 +48,7 @@ public class AdminStudentTab implements Initializable {
 
 
     private void radioButtonSetup(){
+        this.male.setSelected(true);
         this.male.setToggleGroup(toggleGroup);
         this.female.setToggleGroup(toggleGroup);
     }
@@ -69,6 +71,26 @@ public class AdminStudentTab implements Initializable {
             }
         }
 
+        String message = null;
+
+        if(firstName.length() < 4){
+            message = "First Name must be more then 4 char long";
+        }else if(lastName.length() < 2){
+            message = "Last Name must be more than 2 char long";
+        }else if(mobile.length() < 10){
+            message  = "Enter valid contact number ";
+        }
+
+        if(message != null ){
+            Notifications.create()
+                    .title("Fill Student Form Correctly")
+                    .text(message)
+                    .showError();
+
+            return;
+        }
+
+        // save code will be here
         System.out.println(firstName  + "\n " +
                 lastName + "\n " +
                 mobile + "\n " +
