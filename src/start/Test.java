5c8c99874b9954d4104fb1008f4ef9388d69996e
Virendra <patelvirendra62@gmail.com>
2020-04-19 19:26:27 +0530

    Update Test.java

--- src/start/Test.java
+++ src/start/Test.java
@@ -8,13 +8,6 @@ import models.Quiz;
 
 public class Test {
     public static void main(String[] args) {
-      if(
-              (args[0].equals("Wipro") && args[1].equals("Banglore")) ||
-              (args[0].equals("ABC") && args[1].equals("Mumbai"))
-      )
-        System.out.println(args[0] + " technogoies " + args[1]);
-    els e
-        System.out.println("Invalid");
-
+        
     }
 }
