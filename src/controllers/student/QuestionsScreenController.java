585f3be0ab4d33c719851cacf288a409cc14cd5e
Virendra <patelvirendra62@gmail.com>
2020-04-27 23:50:45 +0530

    progress design

--- src/controllers/student/QuestionsScreenController.java
+++ src/controllers/student/QuestionsScreenController.java
@@ -3,22 +3,52 @@ package controllers.student;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXRadioButton;
 import javafx.beans.InvalidationListener;
+import javafx.beans.property.Property;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableStringValue;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.FlowPane;
 import models.Question;
 import models.Quiz;
 import org.controlsfx.control.Notifications;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.ResourceBundle;
+
+
+
 public class QuestionsScreenController implements Initializable {
+
+    public FlowPane progressPane;
+
+    private class QuestionsObservable{
+        Property<String> question = new SimpleStringProperty();
+        Property<String> option1 = new SimpleStringProperty();
+        Property<String> option2 = new SimpleStringProperty();
+        Property<String> option3 = new SimpleStringProperty();
+        Property<String> option4 = new SimpleStringProperty();
+        Property<String> answer = new SimpleStringProperty();
+
+        public void setQuestion(Question question) {
+            this.question.setValue(question.getQuestion());
+            this.option1.setValue(question.getOption1());
+            this.option2.setValue(question.getOption2());
+            this.option3.setValue(question.getOption3());
+            this.option4.setValue(question.getOption4());
+            this.answer.setValue(question.getAnswer());
+        }
+    }
 
     public Label title;
     public Label time;
@@ -35,6 +65,7 @@ public class QuestionsScreenController implements Initializable {
     private List<Question> questionList;
     private  Question currentQuestion;
     int currentIndex = 0 ;
+    private  QuestionsObservable questionsObservable;
 
     public void setQuiz(Quiz quiz) {
         this.quiz = quiz;
@@ -54,6 +85,32 @@ public class QuestionsScreenController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         this.showNextQuestionButton();
         this.hideSubmitQuizButton();
+
+        this.questionsObservable = new QuestionsObservable();
+        bindFields();
+
+
+
+        for(int i = 0 ; i < 20 ; i ++){
+            FXMLLoader fxmlLoader = new FXMLLoader(
+                    getClass()
+                            .getResource("/fxml/student/ProgressCircleFXML.fxml"));
+            try {
+                Node node = fxmlLoader.load();
+                progressPane.getChildren().add(node);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    private void bindFields(){
+        this.question.textProperty().bind(this.questionsObservable.question);
+        this.option4.textProperty().bind(this.questionsObservable.option4);
+        this.option3.textProperty().bind(this.questionsObservable.option3);
+        this.option2.textProperty().bind(this.questionsObservable.option2);
+        this.option1.textProperty().bind(this.questionsObservable.option3);
     }
 
     public void nextQuestions(ActionEvent actionEvent) {
@@ -69,11 +126,18 @@ public class QuestionsScreenController implements Initializable {
             options.add(this.currentQuestion.getOption4());
             Collections.shuffle(options);
 
-            this.question.setText(this.currentQuestion.getQuestion());
-            this.option1.setText(options.get(0));
-            this.option2.setText(options.get(1));
-            this.option3.setText(options.get(2));
-            this.option4.setText(options.get(3));
+            this.currentQuestion.setOption1(options.get(0));
+            this.currentQuestion.setOption2(options.get(1));
+            this.currentQuestion.setOption3(options.get(2));
+            this.currentQuestion.setOption4(options.get(3));
+
+//            this.question.setText(this.currentQuestion.getQuestion());
+//            this.option1.setText(options.get(0));
+//            this.option2.setText(options.get(1));
+//            this.option3.setText(options.get(2));
+//            this.option4.setText(options.get(3));
+
+            this.questionsObservable.setQuestion(this.currentQuestion);
             currentIndex++;
         }else{
             hideNextQuestionButton();
